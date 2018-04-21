c90953e561f56ee92989898e4edea3db6e12a3b7
Unknown <AlexSabourinDev@gmail.com>
2018-04-21 12:13:26 -0400

    First push of Jezz Ball clone to git.
    I already have balls flying around, now I have to add the shooting of a wire and my own graphics.

---
+++ Playground.bat
@@ -0,0 +1,4 @@
+cd C:\Users\Alexandre\Desktop\Development\NES\NESASM3
+NESASM3 ../NesGame/Engine.nesasm
+pause
+start C:\Users\Alexandre\Desktop\Development\NES\fceux-2.2.3-win32\fceux.exe "../NesGame/Engine.nes"
